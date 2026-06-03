e967243bb65e1bbc15b5334731678d868b4a253c
Singhashish-commits <singhashish.cs@gmail.com>
2026-06-04 03:15:31 +0530

    Added The Audit-Service Module

---
+++ Audit-Service/src/main/java/com/ashish/claimbridge/auditservice/AuditServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.claimbridge.auditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditServiceApplication.class, args);
+    }
+
+}
---
+++ Audit-Service/src/test/java/com/ashish/claimbridge/auditservice/AuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.claimbridge.auditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
